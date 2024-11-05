c0b5605d8d334f0f930831920469decedab86a63
janitha479 <134924639+janitha479@users.noreply.github.com>
2024-11-06 00:13:45 +0530

    simple api crud oparations
    Oparations created view students, view student by id, delete student and update student

---
+++ app/Http/Controllers/studentController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\student_we;
+use Illuminate\Http\Request;
+
+
+class studentController extends Controller
+{
+    function viewStd(){
+        $data = student_we::all();
+        return response()->json(['student_details',$data],200);
+    }
+
+    function viewStdById($id){
+        $data = student_we::find($id);
+        if(!$data){
+        return response()->json(['Error'=>'Student Id is Not Found'],404);
+    }else{
+        return response()->json(['ID Found',$data],200);
+    }
+    }
+
+    function deleteStd($id){
+        $data = student_we::find($id);
+        if(!$data){
+            return response()->json(['ID NOt Found',$data],404);
+        }else{
+            $data->delete();
+            return response()->json(['Status'=>'Student Deleted Successfully'],200);
+        }
+    }
+
+
+    function storeStd(request $request){
+        $data = new student_we;
+        $data->name =$request->name;
+        $data->nic =$request->nic; 
+        $data->course =$request->course; 
+        $data->contact =$request->contact;
+         $data->save();
+         return response()->json(['Status'=>'Student Added successfully'],200);
+
+    }
+
+
+    function Updatestd(request $request , $id){
+        $data = student_we::find( $id);
+        if(!$data){
+            return response()->json(['Status'=> 'ID Not Found'],404);
+        }else{
+        $data->name =$request->name ?? $data->name;
+        $data->nic =$request->nic ?? $data->nic; 
+        $data->course =$request->course ?? $data->course; 
+        $data->contact =$request->contact ?? $data->contact;
+        $data->save();
+
+         return response()->json(['Status'=>'Student Details Update successfully','Updated details'=> $data],200);
+        }
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\studentController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/show-std',[studentController::class,'viewStd']);
+Route::get('/show-std-byID/{id}',[studentController::class,'viewStdById']);
+Route::delete('/delete-std/{id}',[studentController::class,'deleteStd']);
+Route::put('/update-std/{id}',[studentController::class,'Updatestd']);
+
+
+
